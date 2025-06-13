b5c03b129d5426a247bc96e6680827ac4963e558
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-06-13 18:47:38 +0530

    [Anuj Chaturvedi] add. check Spring Season by Validating month and date

---
+++ Control_flow/SpringSeason.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SpringSeason{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Month : ");
+int month = input.nextInt();
+System.out.print("Day : ");
+int day = input.nextInt();
+if((month == 3 && day >= 20) || (month == 4) || (month == 5) || (month == 6 && day <= 20)){
+System.out.println("Its a Spring Season");
+}
+else{
+System.out.println("Not a Spring Season");
+}
+input.close();
+}
+}
