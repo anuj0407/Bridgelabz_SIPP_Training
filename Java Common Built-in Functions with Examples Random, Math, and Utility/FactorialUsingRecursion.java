f333d01628ce8bc6ae8009f9d0401bda9cdf8cd1
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-06-19 19:47:13 +0530

    [Anuj Chaturvedi] add. Factorial of a number by recursion

---
+++ Java Common Built-in Functions with Examples Random, Math, and Utility/FactorialUsingRecursion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class FactorialUsingRecursion{
+
+static Scanner input = new Scanner(System.in);
+
+static int inputNumber(){
+System.out.print("Enter Number: ");
+int number = input.nextInt();
+return number;
+}
+
+static int factorial(int number){
+if(number<=1){
+return 1;
+}
+
+factorial(number-1);
+return number*factorial(number-1);
+
+}
+
+public static void main(String[] args){
+int number = inputNumber();
+int result = factorial(number);
+System.out.println("Factorial of "+number + " is " + result);
+}
+
+
+
+}
