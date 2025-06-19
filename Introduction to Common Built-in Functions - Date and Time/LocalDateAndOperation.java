cc088942dab2367e632ef687927097c7c836a2dd
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-06-19 11:30:07 +0530

    [Anuj Chaturvedi] add. Update local date as instructed

---
+++ Introduction to Common Built-in Functions - Date and Time/LocalDateAndOperation.java
@@ -0,0 +1,25 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.util.*;
+
+public class LocalDateAndOperation {
+    public static void main(String[] args) {
+	Scanner input =new Scanner(System.in);
+	System.out.print("Enter Date: (in yyyy-MM-dd) : ");
+	String dateString = input.next();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	LocalDate date = LocalDate.parse(dateString,formatter);
+	date = date.plusDays(07);
+        date = date.plusMonths(01);
+	date = date.plusYears(02);
+	date = date.minusWeeks(03);
+	System.out.println("Updated date is : "+ date);
+
+	
+	
+
+            }
+}
