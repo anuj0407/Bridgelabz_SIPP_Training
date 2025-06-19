5f2fbabc7a7b223a37fe05b731aa9bb65b529291
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-06-19 17:11:32 +0530

    [Anuj Chaturvedi] add. Fibonacci sequence till n'th number

---
+++ Java Common Built-in Functions with Examples Random, Math, and Utility/FibonacciGenerator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class FibonacciGenerator{
+
+static Scanner input = new Scanner(System.in);
+
+static int inputNumber(){
+System.out.print("Enter number of term :");
+int n = input.nextInt();
+return n;
+}
+
+static void fibonacci(int n, int first ,int second){
+if(n == 1){
+System.out.print(first);
+}
+else if(n == 2){
+System.out.print(first +" "+ second);
+}
+else{
+for(int i=1; i<=n ;i++){
+System.out.print(first+" ");
+int temp = first;
+first = second ;
+second = temp + second;
+}
+}
+}
+
+public static void main(String[] args){
+int n = inputNumber();
+fibonacci(n,0,1);
+}
+
+
+
+}
