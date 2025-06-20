a96795dba05e69588f9d826ec206a4b6ae719026
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-06-20 09:56:33 +0530

    [Anuj Chaturvedi] add. Check vowel and consonants and thier frequency in astring by method

---
+++ String/VowelsAndConsonants.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class VowelsAndConsonants{
+
+static int[] vowelConsonants(String str){
+String newStr = "";
+for(int i=0; i<str.length() ;i ++){
+if((int)(str.charAt(i)) < 97){
+int ascii = (int)str.charAt(i) + 32;
+char ch = (char)ascii;
+newStr += ch;
+}
+else{
+newStr += str.charAt(i);
+}
+}
+
+int vowelCounter = 0;
+int consonantCounter = 0;
+int [] vowelAndConsonantsCount = new int[2];
+for(int i=0;i<newStr.length();i++){
+if(newStr.charAt(i) == 'a' || newStr.charAt(i) == 'e' || newStr.charAt(i) == 'i' || newStr.charAt(i) == 'o' || newStr.charAt(i) == 'u'){
+vowelCounter++;
+System.out.println((i+1) + "th character of String is vowel " );
+}
+else{
+consonantCounter++;
+System.out.println((i+1) + "th character of String is consonant " );
+}
+}
+vowelAndConsonantsCount[0] = vowelCounter ; // vowel counts store in 0 index
+vowelAndConsonantsCount[1] = consonantCounter ; // consonant counts store in 1 index
+return vowelAndConsonantsCount;
+}
+
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter string: ");
+String str = input.nextLine();
+int [] result = vowelConsonants(str);
+System.out.println("Number of vowels in given string is :"+result[0]);
+System.out.println("Number of consonants in given string is :"+result[1]);
+}
+
+}
