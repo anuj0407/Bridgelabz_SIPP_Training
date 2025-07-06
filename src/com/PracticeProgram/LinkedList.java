77922c0a015634449d23ad11aac068edd9e6036e
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2025-07-06 14:37:22 +0530

    [Anuj Chaturvedi] add. Banking System with OOPS principle

---
+++ src/com/PracticeProgram/LinkedList.java
@@ -0,0 +1,18 @@
+class Node{ 
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+
+
+
+public class LinkedList{
+
+    public static void main(String[] args){
+        Node a =new Node(10);
+        System.out.println(a.data);
+        System.out.println(a.next);
+    }
+}
